93d7cf46be0f359713564da4834950b0531d8ed3
FrodriguezSandez <frodriguezsandez@hotmail.com>
2022-10-15 22:57:56 -0300

    images 2

--- preprocess.py
+++ preprocess.py
@@ -5,11 +5,13 @@ path = "./dataset/"
 
 dir = os.listdir(path)
 print(len(dir))
+
 for folder in dir:
-  pass
+  print(folder)
   if os.listdir(path + folder)[0].endswith("positive"):
-    org = path + folder + "/study1_positive/image1.png"
+    org = path + folder + "/study1_positive/"
+    if len(os.listdir(org))>1:
+      org += "image2.png"
     dst = path + folder.replace("patient", "") +".png"
     print(org + "  moved to " + dst)
     shutil.move(org, dst)
-

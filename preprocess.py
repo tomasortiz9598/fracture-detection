e96ab0d13d8a244f3e8efa19f33c0a73046c7ee6
tomas ortiz <tomasortiz@MacBook-Pro-de-tomas.local>
2022-10-15 21:48:23 -0300

    images positives

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,12 @@ import shutil
 path = "./dataset/"
 
 dir = os.listdir(path)
+print(len(dir))
 for folder in dir:
-  if os.listdir(path +folder)[0].endswith("negative"):
-    print(path+folder + "   deleted")
-    shutil.rmtree(path+folder)
+  pass
+  if os.listdir(path + folder)[0].endswith("positive"):
+    org = path + folder + "/study1_positive/image1.png"
+    dst = path + folder.replace("patient", "") +".png"
+    print(org + "  moved to " + dst)
+    shutil.move(org, dst)
+
